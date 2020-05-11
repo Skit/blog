4de5c165fe254ffe4736881d3363b4307585fc89
Skit <wabto@mail.ru>
2020-05-11 12:30:46 +0300

    Comment test created

--- blog/tests/unit/comment/CommentCreateTest.php
+++ blog/tests/unit/comment/CommentCreateTest.php
@@ -9,9 +9,13 @@ use blog\entities\post\Comment;
 use blog\entities\user\User;
 use blog\managers\CommentManager;
 use blog\repositories\comment\CommentRepository;
+use blog\repositories\post\PostRepository;
 use blog\repositories\users\UsersRepository;
 use blog\services\CommentService;
 use Codeception\Test\Unit;
+use common\fixtures\ActiveCategoriesFixture;
+use common\fixtures\ActiveCommentsFixture;
+use common\fixtures\PostFixture;
 use common\fixtures\UserFixture;
 use common\fixtures\UserProfilesFixture;
 use Faker\Factory;
@@ -41,7 +45,7 @@ class CommentCreateTest extends Unit
         $this->faker = Factory::create();
         $this->repository = new CommentRepository(Yii::$app->db);
         $this->userRepository = new UsersRepository(Yii::$app->db);
-        $this->manager = new CommentManager($this->repository, new CommentService());
+        $this->manager = new CommentManager($this->repository, $this->userRepository, new PostRepository(Yii::$app->db), new CommentService());
 
         return parent::_setUp();
     }
@@ -56,26 +60,59 @@ class CommentCreateTest extends Unit
             'profile' => [
                 'class' => UserProfilesFixture::class,
                 'dataFile' => codecept_data_dir() . 'profile.php'
+            ],
+            'category' => [
+                'class' => ActiveCategoriesFixture::class,
+                'dataFile' => codecept_data_dir() . 'activeCategories.php'
+            ],
+            'post' => [
+                'class' => PostFixture::class,
+                'dataFile' => codecept_data_dir() . 'post.php'
+            ],
+            'comments' => [
+                'class' => ActiveCommentsFixture::class,
+                'dataFile' => codecept_data_dir() . 'activeComments.php'
             ]
         ]);
     }
 
     public function testFullCreate()
     {
+        $user = $this->tester->grabFixture('user', 0);
+        $post = $this->tester->grabFixture('post', 0);
+
         // TODO все тесты репов переделать на фикстуры. не нужно тут созавать объекты. Подобные тесты сделать на сущностях
         $form = new CommentForm();
         $form->content = $this->faker->text(255);
+        $form->post_id = $post->id;
         $form->parent_id = 0;
+        $form->creator_id = $user->id;
         $form->status = Comment::STATUS_ACTIVE;
 
-        $user = $this->tester->grabFixture('user', 0);
-        $user = $this->userRepository->findOneById($user->id, $user->status);
-
-        $comment = $this->manager->create($user, $form);
+        $comment = $this->manager->create($form);
         $comment = $this->repository->findOneById($comment->getPrimaryKey(), $form->status);
 
         expect($comment->getContent())->equals($form->content);
         expect($comment->getParent()->getPrimaryKey())->equals(0);
-        expect($comment->getCreator()->getUsername())->equals($user->getUsername());
+        expect($comment->getCreator()->getUsername())->equals($user->username);
+    }
+
+    public function testReply()
+    {
+        $user = $this->tester->grabFixture('user', 1);
+        $comment = $this->tester->grabFixture('comments', 1);
+
+        $replyForm = new CommentForm();
+        $replyForm->parent_id = $comment->id;
+        $replyForm->post_id = $comment->post_id;
+        $replyForm->creator_id = $user->id;
+        $replyForm->content = 'reply text';
+        $replyForm->status = Comment::STATUS_ACTIVE;
+
+        $reply = $this->manager->create($replyForm);
+
+        expect($reply->getContent())->equals($replyForm->content);
+        expect($reply->getParent()->getContent())->equals($comment->content);
+        expect($reply->getCreator()->getUsername())->equals($user->username);
     }
 }
